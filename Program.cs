62a9df4b7111b35d2dbadfc5faa127b8745ce7a7
Labden <nedbal.trent@gmail.com>
2021-06-03 11:43:06 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Github_Merge
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Trent");
+
         }
     }
 }
