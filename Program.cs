835759ea92c8e84942ad19e17d3eea6c9e62a029
Labden <nedbal.trent@gmail.com>
2021-06-03 11:08:19 -0400

    Github Merge initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Github_Merge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
